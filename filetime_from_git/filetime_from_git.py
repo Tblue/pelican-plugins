45c2a73b5ee3a62f3b0ebc8cff46f106df4a64f2
Tilman Blumenbach <tilman+git@ax86.net>
2016-02-03 20:00:45 +0100

    Add GIT_FILETIME_ONLY_IF_MISSING setting.
    This will only set metadata fields from Git if they are not already
    present on a content piece; e. g. if they are already set in the
    source file, then they will be preserved.
    
    Signed-off-by: Tilman Blumenbach <tilman+git@ax86.net>

--- filetime_from_git/filetime_from_git.py
+++ filetime_from_git/filetime_from_git.py
@@ -30,11 +30,27 @@ def filetime_from_git(content):
     git = git_wrapper('.')
     tz_name = content.settings.get('TIMEZONE', None)
 
+    # Do we want to retrieve the filetimes from Git?
+    #
+    # 1. The plugin can be disabled for a single content piece: If the content has a metadata field
+    #    named "gittime" and this field is set to "off", "no" or "false" (case-insensitive), then
+    #    we are not touching anything.
     gittime = content.metadata.get('gittime', 'yes').lower()
     gittime = gittime.replace("false", "no").replace("off", "no")
     if gittime == "no":
         logger.debug("Plugin explicitly disabled for `%s'", content.source_path)
         return
+
+    # 2. Optionally, we only update those date fields which are not yet present.
+    #    In other words, preserve the `date' and `modified' fields if they exist and fill them in
+    #    from Git if they don't.
+    only_missing_fields = content.settings.get("GIT_FILETIME_ONLY_IF_MISSING", False)
+    if only_missing_fields and hasattr(content, "date") and hasattr(content, "modified"):
+        logger.debug("`date' and `modified' fields are set for `%s', nothing to do.". content.source_path)
+        return
+
+    date = None
+    modified = None
 
     # 1. file is not managed by git
     #    date: fs time
@@ -51,32 +67,46 @@ def filetime_from_git(content):
 
         if len(commits) == 0:
             # never commited, but staged
-            content.date = git.datetime_from_timestamp(
+            date = datetime_from_timestamp(
                 os.stat(path).st_ctime, content)
         else:
             # has commited
-            content.date = git.get_commit_date(
+            date = git.get_commit_date(
                 commits[-1], tz_name)
 
             if git.is_file_modified(path):
                 # file has changed
-                content.modified = datetime_from_timestamp(
+                modified = datetime_from_timestamp(
                     os.stat(path).st_ctime, content)
             else:
                 # file is not changed
                 if len(commits) > 1:
-                    content.modified = git.get_commit_date(
+                    modified = git.get_commit_date(
                         commits[0], tz_name)
     else:
         # file is not managed by git
-        content.date = datetime_from_timestamp(os.stat(path).st_ctime, content)
+        date = datetime_from_timestamp(os.stat(path).st_ctime, content)
+
+    if date is not None and (not hasattr(content, "date") or not only_missing_fields):
+        # We got a creation date and the content either has no creation date set yet or
+        # we want to override it.
+        logger.debug("Setting `date' field from Git for `%s'", content.source_path)
+        content.date = date
+
+    if modified is not None and (not hasattr(content, "modified") or not only_missing_fields):
+        # We got a modification date and the content either has no modification date set yet or
+        # we want to override it.
+        logger.debug("Setting `modified' field from Git for `%s'", content.source_path)
+        content.modified = modified
 
     if not hasattr(content, 'modified'):
         content.modified = content.date
 
+    # Make sure the `locale_date' field matches the `date' field:
     if hasattr(content, 'date'):
         content.locale_date = strftime(content.date, content.date_format)
 
+    # Make sure the `locale_modified' field matches the `modified' field:
     if hasattr(content, 'modified'):
         content.locale_modified = strftime(
             content.modified, content.date_format)
